c53f2aefd61d90a8ce2850ef41434c236709d485
BJ Paat <brpaat@ayannah.com>
2017-05-29 15:36:34 +0800

    Add RH external

---
+++ external/rh.py
@@ -0,0 +1,27 @@
+import requests
+import json
+
+from envparse import env
+env.read_envfile("local.env")
+
+
+class HubGet():
+    """
+    Remittance Hub API wrapper
+    """
+
+    def __init__(self, **kwargs):
+        self.authorization = kwargs['authorization']
+        self.source_reference_number = kwargs['source_reference_number']
+        self.partner_id = kwargs['partner_id']
+
+    def get_remittance(self):
+        url = env("RH_URL")
+        payload = {
+                "partner_id": self.partner_id,
+                "source_reference_number": self.source_reference_number}
+        headers = {
+                "content-type": "application/json",
+                "AUTHORIZATION": self.authorization}
+        response = requests.post(url, data=json.dumps(payload), headers=headers)
+        return response.json()
